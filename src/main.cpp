bf37046f75142446b2688cec45cfc50a4e542899
Adam Buchowski <adam.buchowski1@gmail.com>
2025-06-25 17:33:24 +0200

    updated main example

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,82 @@
-#include <GLFW/glfw3.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "Input/KeyboardHandler.hpp"
 #include "Input/CallbackHandler.hpp"
 #include "Window.hpp"
+#include "Shader.hpp"
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <print>
 
 static void error_callback(int error, const char *description) {
   fputs(description, stderr);
 }
 
-void drawTriangle(float ratio) {
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-  glBegin(GL_TRIANGLES);
-  glColor3f(1.f, 0.f, 0.f);
-  glVertex3f(-0.6f, -0.4f, 0.f);
-  glColor3f(0.f, 1.f, 0.f);
-  glVertex3f(0.6f, -0.4f, 0.f);
-  glColor3f(0.f, 0.f, 1.f);
-  glVertex3f(0.f, 0.6f, 0.f);
-  glEnd();
-}
-
 int main(void) {
   Window window("Simple example", 800, 600);
   glfwSetErrorCallback(error_callback);
+
+  if (!gladLoadGL(glfwGetProcAddress)) {
+    std::print("Failed to initialize glad");
+    return -1;
+  }
+
   // Input
   Input::KeyboardHandler keyboardHandler;
   Input::CallbackHandler::setActiveKeyboardHandler(keyboardHandler);
   glfwSetKeyCallback(window.getWindow(), Input::CallbackHandler::KeyCallback);
 
-  keyboardHandler.bind(Input::Mapping(Input::Key::Escape),
-                    [&](void) { window.close(); });
+  keyboardHandler.bind(Input::Mapping(Input::Key::Escape), [&](void) { window.close(); });
+
+  // Triangle
+  float vertices[] = {
+         0.0f,  0.5f, 0.0f, 
+        -0.5f, -0.5f, 0.0f, 
+         1.0f, -0.5f, 0.0f  
+    };
+
+  unsigned int VAO, VBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  // Shaders
+  Shader *vertexShader = Shader::loadFromFile("../resources/triangle_vert.glsl", ShaderType::Vertex);
+  Shader *fragmentShader = Shader::loadFromFile("../resources/triangle_frag.glsl", ShaderType::Fragment);
+
+  vertexShader->compile();
+  fragmentShader->compile();
+
+  ShaderProgram shaderProgram({vertexShader, fragmentShader});
+
+  delete vertexShader;
+  delete fragmentShader;
 
   while (window.isRunning()) {
-    float ratio;
     int width, height;
     glfwGetFramebufferSize(window.getWindow(), &width, &height);
-    ratio = width / (float)height;
     glViewport(0, 0, width, height);
 
+    float time = static_cast<float>(glfwGetTime());
+    glm::mat4 transform = glm::rotate(glm::mat4(1.0f), time, glm::vec3(0.0f, 0.0f, 1.0f));
+    shaderProgram.setMat("transform", transform);
+
     window.clear();
-    drawTriangle(ratio);
+
+    shaderProgram.use();
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    
     window.display();
 
     glfwPollEvents();
