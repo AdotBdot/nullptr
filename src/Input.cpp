40bd380108e7fb930844767624cd8b5060517d6e
AdotBdot <adam.buchowski1@gmail.com>
2025-06-25 12:10:00 +0200

    renamed file 3 (fuck)

--- src/Input.cpp
+++ src/Input.cpp
@@ -1,4 +1,4 @@
-#include "InputHandler.hpp"
+#include "Input.hpp"
 
 #include <print>
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "InputHandler.hpp"
+#include "Input.hpp"
 #include "Window.hpp"
 static void error_callback(int error, const char *description) {
   fputs(description, stderr);
